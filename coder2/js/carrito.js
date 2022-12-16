0004277b578580dab94728c2f0517178066f2e96
cachi <hugo_0989m@hotmail.com>
2022-12-16 15:04:16 -0300

    entrega final1

---
+++ coder2/js/carrito.js
@@ -0,0 +1,79 @@
+//creamos el modal del carrito, header, boton y contenido
+const pintarCarrito = () => {
+    modalCaja.innerHTML = "";
+    modalCaja.style.display = "flex"
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML =`
+    <h1 class="modal-header-titulo">Carrito</h1>
+    `;
+    modalCaja.append(modalHeader);
+
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "X";
+    modalbutton.className = "modal-header-button";
+
+    modalbutton.addEventListener("click", () => {
+        modalCaja.style.display = "none";
+    })
+
+    modalHeader.append(modalbutton);
+//recorremos el carrito para el modal
+    carrito.forEach((item) => {
+    let carritoContenido = document.createElement("div");
+    carritoContenido.className = "modal-contenido"
+    carritoContenido.innerHTML = `
+    <img src = "${item.img}">
+    <h3> ${item.nombre}</h3>
+    <p>${item.precio}$</p>
+    <p>${item.cantidad}u.</p>
+    <p>Total: $${item.cantidad * item.precio}</p>
+    <span class="delete-producto"> X </span>
+    `;
+//contamos con length la cantidad de articulos en el loca
+    modalCaja.append(carritoContenido);
+    console.log(carrito.length);  
+
+    let eliminar = carritoContenido.querySelector(".delete-producto");
+//creamos boton para eliminar producto del modal
+    eliminar.addEventListener("click", () => {
+        eliminarProduto(item.id);
+    })
+
+    });
+//algoritmo para modificar la cantidad reflejada precio x cantidad
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0);
+
+    const totalPago = document.createElement("div");
+    totalPago.className = "total-contenido";
+    totalPago.innerHTML = `Total a pagar: ${total}$`;
+    modalCaja.append(totalPago);
+};
+
+verCarrito.addEventListener("click", pintarCarrito);
+    
+
+const eliminarProduto = (id) => {
+    const fundId = carrito.find((element) => element.id === id);
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== fundId;
+    });
+    carritoCounter();
+    saveLocal();
+    pintarCarrito();
+};
+
+const carritoCounter = () => {
+    cantidadCarrito.style.display = "block";
+
+const carritoLength = carrito.length;
+
+localStorage.setItem("carritoLength", JSON.stringify(carritoLength))
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"))
+};
+
+carritoCounter();
+
+
---
+++ coder2/js/index.js
@@ -0,0 +1,73 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalCaja = document.getElementById("modal-caja");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+let botonTurno = document.getElementById("botonTurno");
+//llamamos al carrito con agregando localStorage
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+//boton con sweet alert para la parte sin terminar
+botonTurno.addEventListener("click", () =>{
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops..me quede sin tiempo',
+        text: 'Tenia preparado un sistema de turnos que no llegue con el tiempo!',
+        footer: '<a href="">Te invito a la pokeapi para usar fetch!</a>'
+      })
+})
+//utilizamos FETCH para llamar los archivos JSON y crear los productos en html
+fetch("./js/productos.json")
+.then ((response) => response.json())
+.then((producto) =>{
+producto.forEach((item) => {
+    let div = document.createElement("div");
+    div.className = "cardShop";
+    div.innerHTML = `
+    <img src="${item.img}" class="card-img-top" alt="${item.id}">
+    <div class="card-body">
+      <h3 class="card-title">${item.nombre}</h3>
+      <p class="card-text price">$${item.precio}</p>
+      </div>
+    `;
+    
+    shopContent.append(div);
+
+//creamos boton de comprar
+    let comprar = document.createElement("button")
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+
+    div.append(comprar);
+
+    comprar.addEventListener("click", () => {
+        // con el metodo some buscamos id repetidos a agregar en carrito
+        const repeat = carrito.some((repeatItem) => repeatItem.id === item.id);
+
+        if (repeat){
+            carrito.map((i) => {
+                if(i.id === item.id){
+                    i.cantidad++;
+                }
+            });
+        } else{
+        //pusheamos los item
+        carrito.push({
+            id: item.id,
+            img: item.img,
+            nombre: item.nombre,
+            precio: item.precio,
+            cantidad: item.cantidad,
+        });
+        }
+        //los sacamos por consola y los guardamos en localStorage
+        console.log(carrito);
+        carritoCounter();
+        saveLocal();
+    });
+});
+});
+ //setItem (localStorage)
+ const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+ };
+
+
